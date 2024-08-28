477a09568d7b3c5488ba5bd8be838b1058cdf3f1
mshi-29 <163647942+mshi-29@users.noreply.github.com>
2024-08-28 20:32:06 +0900

    Delete kadai_002 directory

--- kadai_002/crud/decorators.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-
-def subscription_required(view_func):
-    def _wrapped_view_func(request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.is_subscription_user:
-            return view_func(request, *args, **kwargs)
-        else:
-            return redirect('subscription_required')  # エラーページにリダイレクト
-    return _wrapped_view_func
